c18f3df62b99afa92c3e1dee0cd7be1d20972206
Michael Lim <bl458@cornell.edu>
2020-12-02 17:28:48 +0900

    feat: Implement stack using linkedList

--- src/Stack.java
+++ src/Stack.java
@@ -1,3 +1,5 @@
+import java.util.EmptyStackException;
+
 public class Stack<T> implements Iterable<T> {
     private java.util.LinkedList<T> list = new java.util.LinkedList<T>();
 
@@ -5,18 +7,19 @@ public class Stack<T> implements Iterable<T> {
 
     public Stack(T firstElem) {}
 
-    public int size() {}
+    public int size() {return list.size();}
 
-    public boolean isEmpty() {}
+    public boolean isEmpty() {return list.size() == 0;}
 
-    public void push(T elem) {}
+    public void push(T elem) {list.addLast(elem);}
 
-    public T pop() {}
+    public T pop() {
+        if (isEmpty()) throw new EmptyStackException();
+        return list.removeLast();
+    }
 
-    public T peek() {}
+    public T peek() {return list.peekLast();}
 
     @Override
-    public java.util.Iterator<T> iterator() {
-        return list.iterator();
-    }
+    public java.util.Iterator<T> iterator() {return list.iterator();}
 }
