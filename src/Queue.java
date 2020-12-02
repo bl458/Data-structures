6dad877dd3d4df3726c67042ba7342c96d33e6f4
Michael Lim <bl458@cornell.edu>
2020-12-02 17:57:09 +0900

    feat: Implement queue with linkedList

---
+++ src/Queue.java
@@ -0,0 +1,20 @@
+public class Queue<T> implements Iterable<T> {
+    private java.util.LinkedList<T> list = new java.util.LinkedList<T>();
+
+    public Queue() {}
+    
+    public Queue(T firstElem) {offer(firstElem);}
+
+    public int size() {return list.size();}
+
+    public boolean isEmpty() {return list.size() == 0;}
+
+    public void offer(T elem) {list.addLast(elem);}
+
+    public T peek() {return list.peekFirst();}
+
+    public T poll() {return list.removeFirst();}
+
+    @Override
+    public java.util.Iterator<T> iterator() {return list.iterator();}
+}   
